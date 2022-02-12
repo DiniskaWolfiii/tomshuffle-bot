50b3a06f944e06d1107d10d8e5066d62628347c0
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-02-12 21:43:25 +0100

    Revert "Update voiceStateUpdate.js"
    This reverts commit a94bd72e5d589afee715072d7e21c1885d44b07f.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -31,6 +31,6 @@ module.exports = {
             return;
         }
         if (oldState.channelId === '941422962056265748') return;
-        if (oldState.channel.parentId === '941422858607931442' && oldState.channel.members.size === 0) return oldState.channel.delete();
+        if (oldState.channel.parentId === '941422858607931442' && oldState.channel.members.size === 1) return oldState.channel.delete();
     }
 }
