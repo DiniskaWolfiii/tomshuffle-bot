8abd9d578995e700d6421c6ee39c7f82f1235694
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-02-15 09:16:11 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,6 +4,6 @@ module.exports = {
      * @param {import('discord.js').GuildMember} newMember
      */
     execute(newMember) {
-        if(newMember.user.bot) newMember.roles.add(['941857831043358741', '941858319969189968'], "Auto Role Join")
+        if(!newMember.user.bot) newMember.roles.add(['941857831043358741', '941858319969189968'], "Auto Role Join")
     }
 }
