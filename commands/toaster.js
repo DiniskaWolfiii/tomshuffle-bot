b37f49d5e3d76872e61fe550406a91013931ac08
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-04-11 22:14:06 +0200

    Create toaster.js

---
+++ commands/toaster.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('toaster')
+        .setDescription('Wirf einen Toaster in Gesichter anderer Leute.')
+        .addUserOption(option => 
+            option.name('user')
+            .setDescription('User den du mit einem Toaster bewerfen willst.')
+            .setRequired(true)),
+
+    /**
+     * @param {import('discord.js').Interaction} interaction
+     */
+    async execute(interaction) {
+        const toasterUser = interaction.options.getMember('user');
+        let antworten = [
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+            `*${interaction.user} bewirft ${toasterUser} mit einem Toaster*`,
+        ]
+        for (let i = antworten.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = antworten[i];
+            antworten[i] = antworten[j];
+            antworten[j] = temp;
+        }
+        await interaction.reply(antworten[Math.floor(Math.random() * antworten.length)])
+    },
+};
