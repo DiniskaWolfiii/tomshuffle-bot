1c4b9d30bebfae1d5f12e7338ecc2162781f4699
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-04-20 16:35:18 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -28,6 +28,17 @@ module.exports = {
                 ],
             })
                 .then(newChannel => newState.setChannel(newChannel));
+            newState.guild.channels.create(newState.member.user.username, {
+                type: 'GUILD_TEXT',
+                parent: '941422858607931442',
+                position: 30000,
+                permissionOverwrites: [
+                    {
+                        id: newState.member.guild.id,
+                        deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY']
+                    }
+                ]
+            })
             return;
         }
         if (oldState.channelId === '941422962056265748') return;
