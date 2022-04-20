b615c6896f869bb4c0f5f4d042b9e544024107b6
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-04-20 18:39:48 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -51,7 +51,7 @@ module.exports = {
         }
         if (oldState.channelId === '941422962056265748') return; // Wenn verlassener Kanal der "Kanal erstellen" ist, nichts machen
         if (oldState.channel.parentId === '941422858607931442' && oldState.channel.members.size === 0) {
-            oldState.guild.channels.cache.get(oldState.channelId).delete();
+            oldState.guild.channels.cache.get(c => c.topic === oldState.channelId).delete();
             oldState.channel.delete();
         }
     }
