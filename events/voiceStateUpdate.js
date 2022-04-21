05d7cc284629d02f88b81b4189e5fb8bcf021e6a
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-04-21 17:34:01 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -15,7 +15,7 @@ module.exports = {
         // If VoiceStateUpdate Event is a JOIN
         if (oldState.channelId == null) {
             // Create new VoiceChannel and TextChannel - TextChannel with the topic of the ID from VoiceChannel and return
-            if (newState.channelId === joinToCreate && newState.channel.members.size !== 0) {
+            if (newState.channelId === joinToCreate) {
 
                 newState.guild.channels.create(newState.member.user.username + "'s Voice", {
                     type: 'GUILD_VOICE',
@@ -56,8 +56,7 @@ module.exports = {
             }
             // If User is joined, give him right to send Messages into TextChannel
             if (newState.channel.parentId === joinToCreateParent && newState.channelId !== joinToCreate && newState.channel.members.size !== 0) {
-                let meinKanal = getChannelByTopic(newState);
-                meinKanal.permissionOverwrites.edit(newState.member.id, { VIEW_CHANNEL: true, SEND_MESSAGES: true, READ_MESSAGE_HISTORY: true , EMBED_LINKS: true, ATTACH_FILES: true});
+                giveTextPermission(newState);
                 return;
             }
         } // If VoiceStateUpdate Event is a LEAVE 
@@ -67,7 +66,7 @@ module.exports = {
             // If User has leaved, remove rights to send Messages into TextChannel
             if (oldState.channel.parentId === joinToCreateParent && oldState.channelId !== joinToCreate && oldState.channel.members.size !== 0) {
                 let meinKanal = getChannelByTopic(oldState);
-                meinKanal.permissionOverwrites.edit(oldState.member.id, { VIEW_CHANNEL: false, SEND_MESSAGES: false, READ_MESSAGE_HISTORY: false , EMBED_LINKS: false, ATTACH_FILES: false});
+                meinKanal.permissionOverwrites.edit(oldState.member.id, { VIEW_CHANNEL: false, SEND_MESSAGES: false, READ_MESSAGE_HISTORY: false, EMBED_LINKS: false, ATTACH_FILES: false });
                 return;
             }
             // If last User leaved, Delete the VoiceChannel and TextChannel and return
@@ -80,6 +79,13 @@ module.exports = {
                 return;
             }
         }
+        // If VoiceStateUpdate is a User who switched Channels
+        else {
+            // If User switched from a normal Channel to Temp Channel
+            if (oldState.channel.parentId !== joinToCreateParent && newState.channel.parentId === joinToCreateParent && newState.channelId !== joinToCreate) {
+
+            }
+        }
     }
 }
 
@@ -90,6 +96,7 @@ function getChannelByTopic(channelState) {
     let textKanalMap;
 
     // Weird Stuff for getting ID by topic, idk dont ask me im trash coding
+    // I am too lazy to code the shit in JSONs or Databases
 
     kategorieChannel.forEach(kanal => {
         catKanal = kanal.children;
@@ -100,4 +107,8 @@ function getChannelByTopic(channelState) {
 
     const kanal = channelState.guild.channels.cache.find(c => c.id === firstValue.id);
     return kanal;
+}
+function giveTextPermission(channelState) {
+    let meinKanal = getChannelByTopic(channelState);
+    meinKanal.permissionOverwrites.edit(channelState.member.id, { VIEW_CHANNEL: true, SEND_MESSAGES: true, READ_MESSAGE_HISTORY: true, EMBED_LINKS: true, ATTACH_FILES: true });
 }
