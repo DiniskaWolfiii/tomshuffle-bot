4608c7e6bb4652ef36b2656b68616d80cfbbfd03
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-06-05 21:54:08 +0200

    Create bonk.js

---
+++ commands/bonk.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('bonk')
+        .setDescription('TBonke einen anderen User')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User den du bonken')
+                .setRequired(true)),
+
+    /**
+     * @param {import('discord.js').Interaction} interaction
+     */
+    async execute(interaction) {
+        const bonkUser = interaction.options.getMember('user');
+        let antworten;
+
+        if (bonkUser.user.id === interaction.user.id) {
+            antworten = [
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber...*`,
+                `*${interaction.user} bonkt sich selber... Go to Horny Jail!*`,
+            ]
+        } else {
+            antworten = [
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}...*`,
+                `*${interaction.user} bonkt ${bonkUser}... Go to Horny Jail!*`,
+            ]
+        }
+        let randomNumber = Math.floor(Math.random() * antworten.length);
+        await interaction.reply(antworten[randomNumber])
+
+    },
+};
