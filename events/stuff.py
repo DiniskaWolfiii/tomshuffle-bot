d6be3bb1b4d5426c2f8c89befb36853afacbc67f
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-05-03 18:36:16 +0200

    removed music yt

--- events/stuff.py
+++ events/stuff.py
@@ -18,33 +18,5 @@ class StatusPageUpdate(commands.Cog):
     async def update_status(self):
         requests.get(os.getenv("STATUS_PAGE_URL"))
 
-class YouTubeUpdate(commands.Cog): 
-    def __init__(self, bot):
-        self.bot = bot
-        self.check_new_video.start()
-        self.latest_video_id = None
-
-    def cog_unload(self):
-        self.check_new_video.cancel()
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.check_new_video.start()
-
-    @tasks.loop(minutes=5.0)
-    async def check_new_video(self):
-        youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=os.getenv("GOOGLE_API_KEY"))
-        request = youtube.search().list(part="snippet", channelId="UCHhzdFRBfKCPFt-K0RAlg3g", maxResults=1, order="date")
-        response = request.execute()
-        video_id = response["items"][0]["id"]["videoId"]
-        
-        """if self.latest_video_id is None:
-            self.latest_video_id = video_id"""
-        if self.latest_video_id != video_id:
-            self.latest_video_id = video_id
-            channel = self.bot.get_channel(1002248661893398605)
-            await channel.send(f"https://www.youtube.com/watch?v={video_id}")
-
 def setup(bot):
     bot.add_cog(StatusPageUpdate(bot))
-    bot.add_cog(YouTubeUpdate(bot))
